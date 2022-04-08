37e9357a090874e11a72806f986e8a3079bb5e72
Klemense <klemenseloh@gmail.com>
2022-04-08 01:13:23 -0400

    commit tree 3b3e06ea68a213e3d42dc74d8fdd792eff8bcb4a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,32 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  def index 
+    spices = Spice.all
+    render json: spices
+  end 
+
+  def create 
+    spice = Spice.create(spice_params)
+    render json: spice, status: :created
+  end 
+
+  def update 
+    spice = Spice.find_by(id: params[:id])
+    spice.update(spice_params)
+    render json: spice
+  end 
+
+  def destroy
+    spice = Spice.find_by(id: params[:id])
+    spice.destroy
+    head :no_content 
+  end 
+
+  private 
+
+  def spice_params
+    params.permit(:title, :image, :description, :notes , :rating)
+  end 
+
 end
